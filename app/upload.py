5634edc9e12ccd641d48d6e1b58f8bbeb5094df5
Tom Udding <tomudding@users.noreply.github.com>
2019-05-01 10:59:50 +0200

    Add check if file already exists
    Checks if file already exists. This is based on the name, maybe should be changed to the hash.

--- app/upload.py
+++ app/upload.py
@@ -23,6 +23,7 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            #if (os.path.isfile(filename)):
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            if (os.path.isfile(server.config['UPLOAD_FOLDER'].join(filename))):
+                # throw exception because file with that name already exists
+            file.save(os.path.join(server.config['UPLOAD_FOLDER'], filename))
             return redirect('/')
