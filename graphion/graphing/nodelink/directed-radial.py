10260f68318df276306deeb72a0670a98defa69a
Tom Udding <tomudding@users.noreply.github.com>
2019-05-03 19:37:04 +0200

    Modularisation of graphing module

---
+++ graphion/graphing/nodelink/directed-radial.py
@@ -0,0 +1,14 @@
+"""
+Author(s): Tom Udding
+Created: 2019-05-03
+Edited: 2019-05-03
+"""
+import os
+import math
+import networkx as nx
+import pandas as pd
+
+from graphion import server
+
+def generateDirectedNodeLinkGraph(file):
+    return ""
---
+++ graphion/graphing/nodelink/generator.py
@@ -0,0 +1,6 @@
+"""
+Author(s): Tom Udding
+Created: 2019-05-03
+Edited: 2019-05-03
+"""
+from graphion.graphing.nodelink.directed-radial import generateDirectedNodeLinkGraph
