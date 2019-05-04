c44b80f87e70b6c436d8d334da8a62db80e563bd
Tom Udding <tomudding@users.noreply.github.com>
2019-05-04 23:24:44 +0200

    Improve running time by cleaning imports

--- test.py
+++ test.py
@@ -1,25 +1,28 @@
 from timeit import default_timer as timer
 
-#start = timer()
-import csv
-import holoviews as hv
-import json
-import networkx as nx
+start = timer()
 import pandas as pd
 
-from bokeh.embed import components
-from holoviews.operation.datashader import datashade, bundle_graph
-from holoviews import opts
+from bokeh.document import document
+from csv import Sniffer
+from holoviews.element.graphs import Graph
+from holoviews import opts, renderer, extension
+from json import dumps
+from networkx import from_pandas_adjacency
+from networkx.drawing.layout import circular_layout
+end = timer()
+print(end - start)
 
-hv.extension('bokeh')
+start = timer()
+extension('bokeh')
 defaults = dict(width=400, height=400, padding=0.1)
-hv.opts.defaults(opts.EdgePaths(**defaults), opts.Graph(**defaults), opts.Nodes(**defaults))
-#end = timer()
-#print(end - start)
+opts.defaults(opts.EdgePaths(**defaults), opts.Graph(**defaults), opts.Nodes(**defaults))
+end = timer()
+print(end - start)
 
 start = timer()
 with open("datasets/GephiMatrix_co-citation.csv", 'r') as csvfile:
-    dialect = csv.Sniffer().sniff(csvfile.readline())
+    dialect = Sniffer().sniff(csvfile.readline())
 
 #pd.set_option('display.width', 120)
 #pd.set_option('display.max_columns', 5)
@@ -43,21 +46,25 @@ end = timer()
 print(end - start)
 
 start = timer()
-G = nx.from_pandas_adjacency(df)
+G = from_pandas_adjacency(df)
 #print(nx.info(G))
-graph = hv.Graph.from_networkx(G, nx.layout.circular_layout).opts(directed=True, width=600, height=600, arrowhead_length=0.05)
+graph = Graph.from_networkx(G, circular_layout).opts(directed=True, width=600, height=600, arrowhead_length=0.05)
 end = timer()
 print(end - start)
 
 start = timer()
-circular = bundle_graph(graph)
-plot = datashade(circular, width=800, height=800) * circular.nodes
-renderer = hv.renderer('bokeh')
+#circular = bundle_graph(graph)
+#plot = datashade(circular, width=800, height=800) * circular.nodes
+test = renderer('bokeh').server_doc(graph)
+test = test.to_json()
+print(test)
+#test = render(graph)
 #renderer.save(graph, 'test')
 
-test = renderer.server_doc(plot)
-test.title = "test"
-#json = json.dumps(json_item(test, "radialnodelink"))
+#test = renderer.server_doc(graph)
+#test = renderer.server_doc(plot)
+#test.title = "test"
+#json = dumps(json_item(test, "radialnodelink"))
 #print(json)
 end = timer()
 print(end - start)
