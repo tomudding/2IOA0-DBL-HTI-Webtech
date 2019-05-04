191483c46cea2defdf70922b3d04c627a7b5086e
Tom Udding <tomudding@users.noreply.github.com>
2019-05-04 10:22:37 +0200

    Intermittent update to radial graphing generator

--- graphion/graphing/generator.py
+++ graphion/graphing/generator.py
@@ -4,10 +4,25 @@ Created: 2019-05-03
 Edited: 2019-05-04
 """
 from graphion import server
-from graphion.graphing.nodelink.radial import generateDirectedNodeLinkGraph, generateUndirectedNodeLinkGraph
+from graphion.graphing.nodelink.radial import generateRadialGraph
 
-def generateNodeLinkGraph(type, file):
-    return ""
+import os
+
+def generateNodeLinkGraph(type, file, directed=None):
+    file = getFilePath(file)
+    if type.upper() == "RADIAL":
+        return generateRadialGraph(file, directed)
+    elif type.upper() == "FORCEDIRECTED":
+        return generateForceDirectedGraph(file)
+    elif type.upper() == "HIERARCHICAL":
+        return generateHierarchicalGraph(file)
+    else:
+        # throw exception, invalid input
+        return ""
 
 def generateMatrix(type, file):
     return ""
+
+def getFilePath(file):
+    file = file + '.csv'
+    return os.path.join(server.config['UPLOAD_FOLDER'], file)
--- graphion/graphing/nodelink/radial.py
+++ graphion/graphing/nodelink/radial.py
@@ -3,13 +3,42 @@ Author(s): Tom Udding
 Created: 2019-05-03
 Edited: 2019-05-04
 """
-import os
-import math
-import networkx as nx
-import pandas as pd
+import csv
 
-def generateDirectedNodeLinkGraph(file):
+def generateRadialGraph(file, directed):
+    if directed == False or directed == None:
+        return generateUndirectedRadialGraph(file)
+    else:
+        return generateDirectedRadialGraph(file)
+
+def generateDirectedRadialGraph(file):
     return ""
 
-def generateUndirectedNodeLinkGraph(file):
+def generateUndirectedRadialGraph(file):
     return ""
+
+##############################################################################
+# THESE FUNCTIONS SHOULD PROBABLY BE IN A SEPARATE FILE FOR DATA AGGREGATION #
+##############################################################################
+def processCSVFile(file):
+    for row in getCSVData(file):
+        return ""
+
+# Get header from CSV file row by row
+def getCSVHeader(file):
+    with open(file, 'rb') as csvfile:
+        header = csvfile.readline()
+    return header
+
+# Get data from CSV file row by row
+# This improves memory management
+# and runtime for larger files
+def getCSVData(file):
+    with open(file, 'rb') as csvfile:
+        line = csvfile.readline()                   # get first line
+        dialect = csv.Sniffer().sniff(line)         # sniff dialect of CSV file (delimiter etc.)
+        csvfile.seek(0)                             # return pointer to BOF (beginning of file)
+        datareader = csv.reader(csvfile, dialect)   # create CSV reader with sniffed dialect
+        header = next(datareader)                   # skip header of file
+        for row in datareader:
+            yield row
