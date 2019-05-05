819536b9353a25edf641fb1e81cc66c3011dc239
Steven van den Broek <30909373+Yvee1@users.noreply.github.com>
2019-05-05 17:59:29 +0200

    Update visualise.py

--- graphion/visualise.py
+++ graphion/visualise.py
@@ -4,7 +4,7 @@ Created: 2019-05-01
 Edited: 2019-05-01
 """
 from flask import Blueprint, redirect, render_template
-from graphion.graphing.generator import generateNodeLinkGraph
+from graphion.graphing.generator import generateNodeLinkGraph, generateMatrix
 
 visualiseBlueprint = Blueprint('visualiseBlueprint', __name__, template_folder='templates')
 
