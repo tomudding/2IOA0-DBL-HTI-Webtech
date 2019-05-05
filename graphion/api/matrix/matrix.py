1e04950fa4ad77a130218b5efdcda57f76f24a53
Yvee1 <stevenvdb@live.nl>
2019-05-05 18:59:38 +0200

    small fix

--- graphion/api/matrix/matrix.py
+++ graphion/api/matrix/matrix.py
@@ -8,13 +8,12 @@ from flask import Blueprint
 from json import dumps
 from graphion.graphing.generator import generateMatrix
 
-apiNodeLinkRadialBlueprint = Blueprint('apiNodeLinkRadialBlueprint', __name__, template_folder='templates')
+apiMatrixBlueprint = Blueprint('apiMatrixBlueprint', __name__, template_folder='templates')
 
-@apiNodeLinkRadialBlueprint.route('/api/matrix/', methods=['GET'], strict_slashes=False)
-@apiNodeLinkRadialBlueprint.route('/api/matrix/<file>', methods=['GET'], strict_slashes=False)
-
-def radialNodeLinkAPI(file=None, directed=None):
+@apiMatrixBlueprint.route('/api/matrix/matrix', methods=['GET'], strict_slashes=False)
+@apiMatrixBlueprint.route('/api/matrix/matrix/<file>', methods=['GET'], strict_slashes=False)
+def matrixAPI(file=None):
     if file is None:
         return abort(400)
 
-    return dumps(json_item(generateNodeLinkGraph(file)))
+    return dumps(json_item(generateMatrix(file)))
