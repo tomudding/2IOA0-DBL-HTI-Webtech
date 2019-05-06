25e5abdaef1d9e6bd721c8d9eae18e348bd12178
Yvee1 <stevenvdb@live.nl>
2019-05-06 21:35:11 +0200

    Basic idea of bokeh server implementation

--- graphion/graphing/nodelink/graph.py
+++ graphion/graphing/nodelink/graph.py
@@ -1,17 +1,18 @@
 """
 Author(s): Tom Udding
-Created: 2019-05-04
-Edited: 2019-05-06
+Created: 2019-05-03
+Edited: 2019-05-05
 """
 from bokeh.document.document import Document
 from graphion.graphing.parser import processCSVMatrix
 from holoviews.element.graphs import Graph
 from holoviews import opts, renderer, extension
 from networkx import from_pandas_adjacency
-from networkx.drawing.layout import spring_layout
+from networkx.drawing.layout import circular_layout, spring_layout
+import panel as pn
 
-# Force-Directed Graph Generator
-def generateForceDirectedGraph(file, isDirected):
+# Radial Node-Link Graph Generator
+def generateGraph(file, isDirected):
     if isDirected == None:
         isDirected = False
     df = processCSVMatrix(file)
@@ -26,7 +27,6 @@ def generateJSON(df, isDirected):
     opts.defaults(opts.EdgePaths(**defaults), opts.Graph(**defaults), opts.Nodes(**defaults))
 
     G = from_pandas_adjacency(df)
-    graph = Graph.from_networkx(G, spring_layout).opts(directed=isDirected, width=600, height=600, arrowhead_length=0.0005)
-
-    renderedGraph = renderer('bokeh').server_doc(graph)
-    return renderedGraph.roots[0]
+    graph = Graph.from_networkx(G, circular_layout).opts(directed=isDirected, width=600, height=600, arrowhead_length=0.0005)
+    # Make a panel and widgets with param for choosing a layout
+    return pn.Column(graph)
--- graphion/graphing/nodelink/hierarchical.py
+++
@@ -1,5 +0,0 @@
-"""
-Author(s): Tom Udding
-Created: 2019-05-04
-Edited: 2019-05-04
-"""
