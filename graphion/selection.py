339cee81a27ba6c8836a03bc6664e29e9378e054
Tom Udding <tomudding@users.noreply.github.com>
2019-05-10 01:05:32 +0200

    Implement simple file showcase on selection
    Implements a rather simple file showcase on the selection page. It will show you the original filename (no XSS) and provides you with a link to the visualisation page. Fixes #30.

--- graphion/selection.py
+++ graphion/selection.py
@@ -4,9 +4,23 @@ Created: 2019-05-01
 Edited: 2019-05-01
 """
 from flask import Blueprint, render_template
+from glob import glob
+from graphion import server
+from os import listdir
+from os.path import basename, getctime, join
 
 selectionBlueprint = Blueprint('selectionBlueprint', __name__, template_folder='templates')
 
 @selectionBlueprint.route('/selection', methods=['GET'])
 def selection():
-    return render_template('selection.html')
+    recentlyUploadedFiles = getRecentlyUploaded()
+    return render_template('selection.html', filesList=recentlyUploadedFiles)
+
+def getRecentlyUploaded():
+    listOfFiles = sorted([join(server.config['UPLOAD_FOLDER'], f) for f in listdir(server.config['UPLOAD_FOLDER']) if f.endswith('.info')], key=getctime, reverse=True)[:10]
+    finalListOfFiles = {}
+    for file in listOfFiles:
+        with open(file, 'r') as currentFile:
+            id = basename(currentFile.name)[0:-5]
+            finalListOfFiles[id] = currentFile.readline()
+    return finalListOfFiles
