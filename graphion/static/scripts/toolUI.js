231194adf7d4c6aeea7c8c64dcb122e5378fb736
knokko <knokogator@hotmail.com>
2019-05-27 21:06:53 +0200

    Start with implementing the tool design

---
+++ graphion/static/scripts/toolUI.js
@@ -0,0 +1,19 @@
+function showLeftBar(){
+    document.getElementById('left-bar-shown').hidden = false;
+    document.getElementById('left-bar-hidden').hidden = true;
+}
+
+function hideLeftBar(){
+    document.getElementById('left-bar-shown').hidden = true;
+    document.getElementById('left-bar-hidden').hidden = false;
+}
+
+function showRightBar(){
+    document.getElementById('right-bar-shown').hidden = false;
+    document.getElementById('right-bar-hidden').hidden = true;
+}
+
+function hideRightBar(){
+    document.getElementById('right-bar-shown').hidden = true;
+    document.getElementById('right-bar-hidden').hidden = false;
+}
