c3590028cb8db53255f8b61dff6851edfd4eb218
Yvee1 <stevenvdb@live.nl>
2019-05-27 13:28:06 +0200

    commented out print statements

--- graphion/filtering/edge_weight_selection.py
+++ graphion/filtering/edge_weight_selection.py
@@ -32,7 +32,7 @@ def generate_edge_selection(file, kind = "edge", cutoff_l = 0.6, cutoff_r = 10.0
     adj_matrix = df.to_numpy(copy = True)  #convert dataframe to numpy array for efficiency
 
     adj_matrix_copy = adj_matrix.copy(True) #make a deep copy of the adjacency matrix
-    print(adj_matrix)
+    # print(adj_matrix)
     names = df.columns.tolist()
 
     #edge weight fitering
@@ -44,14 +44,14 @@ def generate_edge_selection(file, kind = "edge", cutoff_l = 0.6, cutoff_r = 10.0
                 adj_matrix_copy[i][j] = 0.0
 
     #print matrix after edge weights are filtered
-    print(adj_matrix_copy)
+    # print(adj_matrix_copy)
 
     del_lst1 = []
     del_lst2 = []
 
     for m in range(len(adj_matrix_copy)):
-        print(sum(adj_matrix_copy[m]))
-        print(adj_matrix_copy[m][m])
+        # print(sum(adj_matrix_copy[m]))
+        # print(adj_matrix_copy[m][m])
         if sum(adj_matrix_copy[m]) - adj_matrix_copy[m][m] == 0.0: #node with index m has total out-degree 0
             #minus the diagonal component to remove self-connecting edges
             del_lst1.append(m)
@@ -61,9 +61,9 @@ def generate_edge_selection(file, kind = "edge", cutoff_l = 0.6, cutoff_r = 10.0
             del_lst2.append(n)
     del_lst = intersection(del_lst1, del_lst2)
 
-    print(del_lst1)
-    print(del_lst2)
-    print(del_lst)
+    # print(del_lst1)
+    # print(del_lst2)
+    # print(del_lst)
 
     #below nodes in the del_lst will be deleted
     c = np.delete(adj_matrix, tuple(del_lst), 0)
@@ -72,12 +72,12 @@ def generate_edge_selection(file, kind = "edge", cutoff_l = 0.6, cutoff_r = 10.0
     filtered_edge_node_matrix = np.delete(d, tuple(del_lst), 1)
 
     #test #########################
-    print(adj_matrix[del_lst[3]][1])
-    print(filtered_node_matrix[del_lst[3]][1])
-    print(adj_matrix[2][del_lst[-1]])
+    # print(adj_matrix[del_lst[3]][1])
+    # print(filtered_node_matrix[del_lst[3]][1])
+    # print(adj_matrix[2][del_lst[-1]])
 ##  aprint(filtered_node_matrix[2][del_lst[-1]])
-    print(filtered_node_matrix)
-    print(filtered_edge_node_matrix)
+    # print(filtered_node_matrix)
+    # print(filtered_edge_node_matrix)
     #################################
 
     rem_lst = [i for i in range(len(adj_matrix)) if i not in del_lst]  # remaining indices
@@ -140,18 +140,18 @@ def generate_degree_selection(file, cutoff_l = 2, cutoff_r = 900, dir = "in"):
     else:
         print("invalid dir value!")
 
-    print(del_lst)
+    # print(del_lst)
     rem_lst = [i for i in range(len(adj_matrix)) if i not in del_lst]  # remaining indices
     rem_names = [names[i] for i in rem_lst]  # search and list the remaining column names with the remaining indices
 
     c = np.delete(adj_matrix, tuple(del_lst), 0)
     result_matrix = np.delete(c, tuple(del_lst), 1)
 
-    print(result_matrix)
+   #  print(result_matrix)
 
     # build the output dataframes
     output_df = pandas.DataFrame(result_matrix, index=rem_names, columns=rem_names)
-    print(output_df)
+    # print(output_df)
 
     return output_df
 
