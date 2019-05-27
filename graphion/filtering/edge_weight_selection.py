b9ffac2b9474f981a0823a506b49cf514177d748
Sophia <SophiaZeng2.718@gmail.com>
2019-05-27 11:18:35 +0200

    Finished implementing degree filtering

--- graphion/filtering/edge_weight_selection.py
+++ graphion/filtering/edge_weight_selection.py
@@ -24,38 +24,21 @@ def processCSVMatrix(file):
 
     return df
 
-def generate_selection(file, kind = "edge", cutoff_l = 0.2, cutoff_r = 10.0, keep_edges = False):
+def generate_edge_selection(file, kind = "edge", cutoff_l = 0.6, cutoff_r = 10.0, keep_edges = False):
     df = processCSVMatrix(file)
     fileUniqueHash ='test1'
     df.to_hdf(os.path.join("../../datasets/", (fileUniqueHash + '.h5')), key=fileUniqueHash)
     df = read_hdf(os.path.join("../../datasets/", (fileUniqueHash + '.h5')))
 
     #if file is already in hdf format, apply the following read method
-    print(df[-20:])
     #df = read_hdf(file)
-
+    # print(df[-20:])
 
     adj_matrix = df.to_numpy(copy = True)  #convert dataframe to numpy array for efficiency
 
     adj_matrix_copy = adj_matrix.copy(True) #make a deep copy of the adjacency matrix
     print(adj_matrix)
     names = df.columns.tolist()
-
-    for name in names:
-        print(name)
-
-    ##print(df[:10])
-    #count = 0
-    #for row in df.itertuples():
-    #    if count < 10:
-    #          print(row)
-    #    for i in range(len(row))[1:]:
-    #        edge_w = row[i]
-    #        if(edge_w < cutoff_l or edge_w > cutoff_r):
-    #            row[i] = 0.0
-    #    count = count + 1
-    #print(df[-10:])
-
 
     #edge weight fitering
     #for all weights outside of the cutoff range, write its value to 0.0
@@ -72,6 +55,8 @@ def generate_selection(file, kind = "edge", cutoff_l = 0.2, cutoff_r = 10.0, kee
     del_lst2 = []
 
     for m in range(len(adj_matrix_copy)):
+        print(sum(adj_matrix_copy[m]))
+        print(adj_matrix_copy[m][m])
         if sum(adj_matrix_copy[m]) - adj_matrix_copy[m][m] == 0.0: #node with index m has total out-degree 0
             #minus the diagonal component to remove self-connecting edges
             del_lst1.append(m)
@@ -80,7 +65,6 @@ def generate_selection(file, kind = "edge", cutoff_l = 0.2, cutoff_r = 10.0, kee
             #minus the diagonal component to remove self-connecting edges
             del_lst2.append(n)
     del_lst = intersection(del_lst1, del_lst2)
-    #rem_lst = [ i in range[len(adj_matrix)] not in del_lst]
 
     print(del_lst1)
     print(del_lst2)
@@ -96,15 +80,22 @@ def generate_selection(file, kind = "edge", cutoff_l = 0.2, cutoff_r = 10.0, kee
     print(adj_matrix[del_lst[3]][1])
     print(filtered_node_matrix[del_lst[3]][1])
     print(adj_matrix[2][del_lst[-1]])
-    print(filtered_node_matrix[2][del_lst[-1]])
+##  aprint(filtered_node_matrix[2][del_lst[-1]])
     print(filtered_node_matrix)
     print(filtered_edge_node_matrix)
     #################################
 
-    if keep_edges:
-        filtered_node_matrix
-    return filtered_edge_node_matrix
+    rem_lst = [i for i in range(len(adj_matrix)) if i not in del_lst]  # remaining indices
+    rem_names = [names[i] for i in rem_lst] # search and list the remaining column names with the remaining indices
 
+    #build the output dataframes
+    df_filtered_keep_edge = pandas.DataFrame(filtered_node_matrix, index = rem_names, columns = rem_names)
+    df_filtered_not_keep_edge = pandas.DataFrame(filtered_edge_node_matrix, index = rem_names, columns = rem_names)
+
+
+    if keep_edges:
+        df_filtered_keep_edge
+    return df_filtered_not_keep_edge
 
 
 def intersection(lst1, lst2):
@@ -112,4 +103,68 @@ def intersection(lst1, lst2):
     lst3 = [value for value in lst1 if value in temp]
     return lst3
 
-generate_selection("../../datasets/GephiMatrix_author_similarity.csv")
+
+def generate_degree_selection(file, cutoff_l = 2, cutoff_r = 900, dir = "in"):
+    df = processCSVMatrix(file)
+    fileUniqueHash = 'test1'
+    df.to_hdf(os.path.join("../../datasets/", (fileUniqueHash + '.h5')), key=fileUniqueHash)
+    df = read_hdf(os.path.join("../../datasets/", (fileUniqueHash + '.h5')))
+
+    # if file is already in hdf format, apply the following read method
+    # df = read_hdf(file)
+
+    adj_matrix = df.to_numpy(copy=True)  # convert dataframe to numpy array for efficiency
+
+    #print(adj_matrix)
+    names = df.columns.tolist()
+
+    # degree weight fitering
+
+    del_lst = []  # initialize list of indices of nodes going to be deleted
+
+    if dir == "out":
+        # for all nodes with out-degree outside of the cutoff range, add them to the delete list
+        for i in range(len(adj_matrix)):  # iterate through rows
+            count = 0  #initialize the count for zero weights
+            for j in range(len(adj_matrix[i])):  # iterate through columns
+                if adj_matrix[i][j] == 0.0 and not i == j: #don't count the diagonal edge
+                    count += 1
+            out_degree = len(adj_matrix[i]) - count #outdegree equals to the remaining none zero columns in given row
+            #print(out_degree)
+            if(out_degree < cutoff_l or out_degree > cutoff_r):
+                del_lst.append(i)
+
+
+    elif dir == "in":
+        # for all nodes with in-degree outside of the cutoff range, add them to the delete list
+        adj_matrix_t = adj_matrix.transpose();
+        for i in range(len(adj_matrix_t)):  # iterate through rows
+            count = 0  # initialize the count for zero weights
+            for j in range(len(adj_matrix_t[i])):  # iterate through columns
+                if adj_matrix_t[i][j] == 0.0 and not i == j: #don't count the diagonal edge
+                    count += 1
+
+            in_degree = len(adj_matrix_t[i]) - count  # indegree equals to the remaining none zero columns in given row
+            print(in_degree)
+            if (in_degree < cutoff_l or in_degree > cutoff_r):
+                del_lst.append(i)
+    else:
+        print("invalid dir value!")
+
+    print(del_lst)
+    rem_lst = [i for i in range(len(adj_matrix)) if i not in del_lst]  # remaining indices
+    rem_names = [names[i] for i in rem_lst]  # search and list the remaining column names with the remaining indices
+
+    c = np.delete(adj_matrix, tuple(del_lst), 0)
+    result_matrix = np.delete(c, tuple(del_lst), 1)
+
+    print(result_matrix)
+
+    # build the output dataframes
+    output_df = pandas.DataFrame(result_matrix, index=rem_names, columns=rem_names)
+    print(output_df)
+
+    return output_df
+
+#generate_edge_selection("../../datasets/GephiMatrix_author_similarity.csv")
+generate_degree_selection("../../datasets/GephiMatrix_author_similarity.csv")
